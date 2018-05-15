f98440cd70c2811b5d4136f98d94fb14f41def14
Jeff Studenski <jstudenski@gmail.com>
2018-05-15 08:49:33 -0500

    css tweaks

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Starter</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
       </div>
     );
