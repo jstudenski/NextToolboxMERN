617b40c65aee2bba058c3249ae4dc2f4999aa106
jstudenski <jstudenski@gmail.com>
2018-05-22 11:11:04 -0500

    functioning messy version

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import 'typeface-roboto';
+import 'normalize.css';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+registerServiceWorker();
