60590792c9379302d1b5791a51ea8726cf8ad756
Jeff Studenski <jstudenski@gmail.com>
2018-05-22 11:21:28 -0500

    route linking

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,6 @@
-// const router = require('express').Router();
-// const sampleRoutes = require('./samples');
-
-// router.use('/samples', sampleRoutes);
-
-// module.exports = router;
-
-
 const router = require('express').Router();
 
-const sampleRoutes = require('./sample');
+const sampleRoutes = require('./sample.route');
 
 router.use('/samples', sampleRoutes);
 
